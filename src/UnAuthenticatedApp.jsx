3ae1b82bfc386a29816b6fbc7523652f8843ea72
Daniel Avalos <daniel_avalos@outlook.com>
2024-07-25 20:56:22 -0600

    ADD UnAuthenticatedApp component

---
+++ src/UnAuthenticatedApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Layout } from "./components/Layout";
+import { LoginForm } from "./components/LoginForm";
+import { Screen } from "./components/Screen";
+import { SignUpForm } from './components/SignupForm';
+import styled from "@emotion/styled";
+import { colors } from "./styles";
+
+const CustomLink = styled.button`
+background: none;
+border: none;
+cursor: pointer;
+&:hover {
+  color: ${colors.pink[400]};
+}
+`;
+
+export const UnAuthenticatedApp = () => {
+  const [showLogin, setShowLogin] = useState(true);
+  const handleClick = e => {
+    e.preventDefault();
+    setShowLogin(!showLogin);
+  }
+  return (
+    <Layout>
+      <Screen>
+        <h1>Welcome to Poke Collection</h1>
+        {
+          showLogin ? <LoginForm /> : <SignUpForm />
+        }
+        <CustomLink onClick={handleClick}>{showLogin ? 'Create Account' : 'Log In'}</CustomLink>
+      </Screen>
+    </Layout>
+  )
+}
