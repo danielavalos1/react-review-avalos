c78268ed27a5f67bbcb44abd6f7122e121d937dc
Daniel Avalos <daniel_avalos@outlook.com>
2024-07-26 12:26:52 -0600

    ADD handleAddFavorite function

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,13 @@
 
 import { Input } from "../components/Input"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { getPokemon } from "../services/pokeapi-service";
 import { PokemonData } from '../components/PokemonData';
+import { createFavorite } from "../services/favorites-services";
 
 export const SearchPage = () => {
   const [query, setQuery] = useState('');
+  const [favorites, setFavorites] = useState([]);
   const [state, setState] = useState({
     status: "idle", //success, error, pending
     data: null,
@@ -31,6 +33,25 @@ export const SearchPage = () => {
         })
       });
   }
+
+  const handleAddFavorite = () => {
+    const data = {
+      pokemon_name: pokemon.name,
+      pokemon_id: pokemon.id,
+      pokemon_type: pokemon.types[0].type.name,
+      pokemon_avatar_url: pokemon.sprites.other['official-artwork'].front_default
+    };
+    createFavorite(data)
+      .then(newFavorite => setFavorites([...favorites, newFavorite]))
+      .catch(error => console.log(error));
+  }
+
+  const isFavorite = favorites.find(pokeFav => pokeFav.pokemon_name === pokemon?.name) ? true : false;
+
+  useEffect(() => {
+    console.log(favorites);
+  }, [favorites]);
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -44,7 +65,7 @@ export const SearchPage = () => {
         <button>Search</button>
       </form>
       {status === "idle" && "Ready to search"}
-      {status === "success" && <PokemonData pokemon={pokemon} />}
+      {status === "success" && <PokemonData onAddFavorite={handleAddFavorite} pokemon={pokemon} isFavorite={isFavorite} />}
       {status === "error" && <p style={{ color: "red" }}>{error}</p>}
     </div>
 
