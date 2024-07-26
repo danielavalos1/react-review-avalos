480cf85668257bace4c82ac0570d4fce2f6a3943
Daniel Avalos <daniel_avalos@outlook.com>
2024-07-25 20:31:51 -0600

    REMOVE Form and add LoginForm and SignupForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,16 @@
-import { useState } from "react";
 import { Layout } from "./components/Layout";
+import { LoginForm } from "./components/LoginForm";
 import { Screen } from "./components/Screen";
-import { Input } from './components/Input';
+import { SignUpForm } from './components/SignupForm';
 
 function App() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  })
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  }
 
 
   return (
     <Layout>
       <Screen>
         <h1>Welcome to Poke Collection</h1>
-        <form onSubmit={handleSubmit}>
-          <Input
-            label={'Email'}
-            type="email"
-            name="email"
-            id="email"
-            placeholder="example@mail.com"
-            onChange={handleChange}
-            value={formData.email}
-          />
-          <Input
-            label={'Password'}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="******"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <button type="submit">Login</button>
-        </form>
+        <SignUpForm />
       </Screen>
     </Layout>
   )
