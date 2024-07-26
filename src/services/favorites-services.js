80c0f097e212eb75d0d6bbffed4024edc7ec8a2f
Daniel Avalos <daniel_avalos@outlook.com>
2024-07-26 12:33:47 -0600

    ADD removeFavorite service

--- src/services/favorites-services.js
+++ src/services/favorites-services.js
@@ -3,3 +3,7 @@ import { apiFetch } from './api-fetch';
 export const createFavorite = (data) => {
   return apiFetch('/favorites', { body: data });
 }
+
+export const removeFavorite = (id) => {
+  return apiFetch(`/favorites/${id}`, { method: "DELETE" });
+}
