2f985763269cf22fb9c56f88c6b6c54a86a930cf
Daniel Avalos <daniel_avalos@outlook.com>
2024-07-26 09:55:38 -0600

    USE PokemonData component and getPokemon service

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,33 @@
 
+import { Input } from "../components/Input"
+import { useState } from "react"
+import { getPokemon } from "../services/pokeapi-service";
+import { PokemonData } from '../components/PokemonData';
 
 export const SearchPage = () => {
+  const [query, setQuery] = useState('');
+  const [pokemon, setPokemon] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon(query)
+      .then(data => setPokemon(data))
+      .catch(error => console.log(error));
+  }
   return (
-    <div>SearchPage</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name={'query'}
+          placeholder={'Pokemon Name'}
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {
+        pokemon ? <PokemonData pokemon={pokemon} /> : 'Ready to search'
+      }
+    </div>
+
   )
 }
